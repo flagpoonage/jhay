6dfdac69a62c6c97c0f262c85c51bed5303428f0
James Hay <pokinoice@live.com>
2020-06-10 15:26:24 +0800

    Fix titles for anchors

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const EXPANDED_STATE = 'data-expanded';
-const EXPANDER_SOURCE = 'data-expand-source';
 
 window.addEventListener('click', ev => {
   const expander_element = ev.composedPath().find(a => a.dataset && a.dataset.expandTarget);
@@ -8,7 +7,7 @@ window.addEventListener('click', ev => {
     return;
   }
 
-  const expander_target = document.querySelector(`[${EXPANDER_SOURCE}=${expander_element.dataset.expandTarget}]`);
+  const expander_target = document.querySelector(`[data-expand-source=${expander_element.dataset.expandTarget}]`);
   
   if (!expander_target) {
     return console.warn(`No expander target found for ${expander_element.dataset.expandTarget}`);
@@ -22,9 +21,13 @@ window.addEventListener('click', ev => {
 function expand (target, source) {
   target.setAttribute(EXPANDED_STATE, '');
   source.setAttribute(EXPANDED_STATE, '');
+  source.setAttribute('title', source.dataset.expandedTitle);
+  source.querySelector('svg title').innerHTML = source.dataset.expandedTitle;
 }
 
 function collapse (target, source) {
   target.removeAttribute(EXPANDED_STATE);
   source.removeAttribute(EXPANDED_STATE);
+  source.setAttribute('title', source.dataset.collapsedTitle);
+  source.querySelector('svg title').innerHTML = source.dataset.collapsedTitle;
 }
